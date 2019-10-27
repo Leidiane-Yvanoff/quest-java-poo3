41311a702434e0264020471f732c8e243d508147
Leidiane YVANOFF <lydesign@MacBook-Pro-de-Leidiane.local>
2019-10-27 16:19:37 +0100

    Interface Fly

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -21,4 +21,60 @@ public class Eagle extends Bird {
     public String sing() {
         return "Screech!";
     }
+    
+    @Override
+    public void takeOff() {
+	  System.out.printf("%s takes off in the sky. %n", this.getName());
+	  
+    }
+    
+    /**
+     * fly ascend
+     * @param meters 
+     * @return altitude
+     */
+    @Override
+    
+    public int ascend(int meters) {
+        this.altitude = meters;
+       
+        System.out.printf("%s flies upward, altitude : %d%n", this.getName(), this.altitude);
+    
+        return this.altitude; 
+  
+    }
+    @Override
+    public void glide()
+    {
+    	System.out.println("It glides into the air.");
+    }
+    
+   
+    /**
+     * fly descend
+     * @param meters 
+     * @return altitude
+     */
+	@Override
+	public int descend(int meters) {
+		this.altitude = meters;
+	       
+        System.out.printf("%s flies downward, altitude : %d%n", this.getName(), this.altitude);
+    
+        return this.altitude; 
+		
+	}
+
+	@Override
+	public void land() {
+		// TODO Auto-generated method stub
+		
+		  System.out.printf("%s lands on the ground. %n", this.getName());
+		  
+    
+		
+	}
+    
+    	
+    
 }
